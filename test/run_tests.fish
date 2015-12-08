f894609ba03e910b007ad47a9489bcccadfca54b
James O'Beirne <james.obeirne@gmail.com>
2015-12-08 09:55:36 -0800

    Add 'run' command, allow shell args to be forwarded in 'go'

--- test/run_tests.fish
+++ test/run_tests.fish
@@ -14,4 +14,8 @@ set CURRENT (env DESK_ENV=$HOME/.desk/desks/hello.fish desk)
 echo $CURRENT | grep "say_hello - Args: <hello_to>. Say hello to someone." >/dev/null
 test $status -ne 0; and echo "say_hello command not found"; and exit 1
 
+set RAN (desk run hello mrfish)
+echo $RAN | grep "Hello mrfish" >/dev/null
+test $status -ne 0; and echo "Desk run with 'hello' failed"; and exit 1
+
 exit 0
