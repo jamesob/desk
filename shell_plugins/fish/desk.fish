3437e037fb3ab63315c8d9ba2b7e4a6e96b3f9d4
Christian Michael Paul Bankester <cbankester@immense.net>
2016-01-16 01:04:20 -0600

    Added desk completions

---
+++ shell_plugins/fish/desk.fish
@@ -0,0 +1,43 @@
+# desk.fish - desk completions for fish shell
+#
+# To install the completions:
+# mkdir -p ~/.config/fish/completions
+# cp desk.fish ~/.config/fish/completions
+
+function __fish_desk_no_subcommand --description 'Test if desk has yet to be given a subcommand'
+    for i in (commandline -opc)
+        if contains -- $i init list ls . go run edit help version
+            return 1
+        end
+    end
+    return 0
+end
+
+# desk
+complete -c desk -f -n '__fish_desk_no_subcommand' -a init -d 'Initialize desk configuration'
+
+# desk list|ls
+complete -c desk -f -n '__fish_desk_no_subcommand' -a "ls" -d 'List all desks along with a description'
+complete -c desk -A -f -n '__fish_seen_subcommand_from list' -l only-names -d 'List only the names of the desks'
+complete -c desk -A -f -n '__fish_seen_subcommand_from list' -l no-format -d "Use ' - ' to separate names from descriptions"
+complete -c desk -A -f -n '__fish_seen_subcommand_from ls' -l only-names -d 'List only the names of the desks'
+complete -c desk -A -f -n '__fish_seen_subcommand_from ls' -l no-format -d "Use ' - ' to separate names from descriptions"
+
+# desk go|.
+complete -c desk -f -n '__fish_desk_no_subcommand' -a "go" -d 'Activate a desk. Extra arguments are passed onto shell'
+complete -c desk -A -f -n '__fish_seen_subcommand_from .' -a "(desk ls --only-names)" -d "Desk"
+complete -c desk -A -f -n '__fish_seen_subcommand_from go' -a "(desk ls --only-names)" -d "Desk"
+
+# desk run
+complete -c desk -f -n '__fish_desk_no_subcommand' -a run -d 'Run a command within a desk\'s environment then exit'
+complete -c desk -A -f -n '__fish_seen_subcommand_from run' -a '(desk ls --only-names)' -d "Desk"
+
+# desk edit
+complete -c desk -f -n '__fish_desk_no_subcommand' -a edit -d 'Edit (or create) a deskfile with the name specified, otherwise edit the active deskfile'
+complete -c desk -A -f -n '__fish_seen_subcommand_from edit' -a '(desk ls --only-names)' -d "Desk"
+
+# desk help
+complete -c desk -f -n '__fish_desk_no_subcommand' -a help -d 'Show help text'
+
+# desk version
+complete -c desk -f -n '__fish_desk_no_subcommand' -a version -d 'Show version information'
